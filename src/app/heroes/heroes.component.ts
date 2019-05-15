1a5cbf4fa6936cd94fd232a8210d9d8d0187f477
chendongdong <>
2019-05-15 13:22:27 +0800

    add hreoes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Hero from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.less']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES
 
   constructor() { }
 
